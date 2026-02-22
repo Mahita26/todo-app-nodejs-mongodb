7e9343464eb484d24918c499f45ade9b93460a5e
Mahita26 <mahitaofficial01@gmail.com>
2026-02-22 13:14:08 +0530

    Converted to Express server

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("My first Node.js server is running!");
+const app = express();
+
+const PORT = 3000;
+
+// Home route
+app.get("/", (req, res) => {
+    res.send("My Express server is running!");
 });
 
-server.listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
