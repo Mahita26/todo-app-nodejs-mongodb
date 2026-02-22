2783aa86f545eb035225e7897341c9be21034fc7
Mahita26 <mahitaofficial01@gmail.com>
2026-02-22 13:23:32 +0530

    Added tasks API endpoint

--- server.js
+++ server.js
@@ -4,9 +4,23 @@ const app = express();
 
 const PORT = 3000;
 
+// Middleware to read JSON
+app.use(express.json());
+
 // Home route
 app.get("/", (req, res) => {
     res.send("My Express server is running!");
+});
+
+// Tasks route (NEW API)
+app.get("/tasks", (req, res) => {
+    const tasks = [
+        { id: 1, task: "Learn Node.js" },
+        { id: 2, task: "Build ToDo App" },
+        { id: 3, task: "Push code to GitHub" }
+    ];
+
+    res.json(tasks);
 });
 
 // Start server
